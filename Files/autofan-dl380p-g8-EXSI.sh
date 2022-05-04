e6059584dd944d33ef1b34ee9e602b8a093d1512
Thomas Wilbur <thomaswilbur@adaclare.com>
2022-05-03 21:29:44 -0500

    Add more fixes to ESXI version

--- Files/autofan-dl380p-g8-EXSI.sh
+++ Files/autofan-dl380p-g8-EXSI.sh
@@ -14,66 +14,67 @@ USERNAME="YOURILOUSER"
 ILOIP="YOURILOIP"
 
 FILE="/usr/bin/sshpass"
+echo "ESXI HP iLO Fan Control Utility - By Thomas Wilbur"
 if [ -f "$FILE" ]; then
     echo "sshpass already loaded."
 else 
 esxcli network firewall ruleset set -e true -r httpClient
 pwdlocation=$(pwd)
 cd /tmp
-wget https://github.com/thomaswilbur/HP-ILO-Fan-Control/blob/main/Files/sshpass?raw=true --no-check-certificate
-mv sshpass?raw=true /usr/bin/sshpass
-chmod +x sshpass
-cd pwdlocation
+wget https://raw.githubusercontent.com/thomaswilbur/HP-ILO-Fan-Control/main/Files/sshpass --no-check-certificate
+mv sshpass /usr/bin/sshpass
+chmod +x /usr/bin/sshpass
+cd $pwdlocation
     echo "sshpass loaded."
 fi
 
 esxcli network firewall ruleset set -e true -r sshClient
 #T1="$(sensors -Aj coretemp-isa-0000 | jq '.[][] | to_entries[] | select(.key | endswith("input")) | .value' | sort -rn | head -n1)"
 #T2="$(sensors -Aj coretemp-isa-0001 | jq '.[][] | to_entries[] | select(.key | endswith("input")) | .value' | sort -rn | head -n1)"
-sshpass -p $PASSWORD ssh -oStrictHostKeyChecking=no -oKexAlgorithms=+diffie-hellman-group14-sha1 $USERNAME@$ILOIP show /system1/sensor2 > temp.txt
-T1CLEAN=$(grep -Ihr "CurrentReading" temp.txt)
+sshpass -p $PASSWORD ssh -oStrictHostKeyChecking=no -oKexAlgorithms=+diffie-hellman-group14-sha1 $USERNAME@$ILOIP show /system1/sensor2 > /tmp/temp.txt
+T1CLEAN=$(grep -Ihr "CurrentReading" /tmp/temp.txt)
 T1=$(echo "${T1CLEAN/    CurrentReading=/}" | xargs)
-rm -rf temp.txt
-sshpass -p $PASSWORD ssh -oStrictHostKeyChecking=no -oKexAlgorithms=+diffie-hellman-group14-sha1 $USERNAME@$ILOIP show /system1/sensor3 > temp.txt
-T2CLEAN=$(grep -Ihr "CurrentReading" temp.txt)
+T1=$(echo $T1 | sed 's/[^0-9]*//g')
+rm -rf /tmp/temp.txt
+
+sshpass -p $PASSWORD ssh -oStrictHostKeyChecking=no -oKexAlgorithms=+diffie-hellman-group14-sha1 $USERNAME@$ILOIP show /system1/sensor3 > /tmp/temp.txt
+T2CLEAN=$(grep -Ihr "CurrentReading" /tmp/temp.txt)
 T2=$(echo "${T2CLEAN/    CurrentReading=/}" | xargs)
-rm -rf temp.txt
-sshpass -p $PASSWORD ssh -oStrictHostKeyChecking=no -oKexAlgorithms=+diffie-hellman-group14-sha1 $USERNAME@$ILOIP show /system1/sensor25 > temp.txt
-T2CLEAN=$(grep -Ihr "CurrentReading" temp.txt)
+T2=$(echo $T2 | sed 's/[^0-9]*//g')
+rm -rf /tmp/temp.txt
+
+sshpass -p $PASSWORD ssh -oStrictHostKeyChecking=no -oKexAlgorithms=+diffie-hellman-group14-sha1 $USERNAME@$ILOIP show /system1/sensor25 > /tmp/temp.txt
+T2CLEAN=$(grep -Ihr "CurrentReading" /tmp/temp.txt)
 RAID=$(echo "${T2CLEAN/    CurrentReading=/}" | xargs)
-rm -rf temp.txt
-T1=${T1//$'\n'/}
-T2=${T2//$'\n'/}
-RAID=${RAID//$'\n'/}
-T1=${T1%$'\n'}
-T2=${T2%$'\n'}
-RAID=${RAID%$'\n'}
+RAID=$(echo $RAID | sed 's/[^0-9]*//g')
+rm -rf /tmp/temp.txt
+
 
 echo "CPU 1 Temp $T1 C"
 
 
-if [[ $T1 > 67 ]]
+if [[ $T1 -gt 67 ]]
    then
         sshpass -p $PASSWORD ssh -oStrictHostKeyChecking=no -oKexAlgorithms=+diffie-hellman-group14-sha1 $USERNAME@$ILOIP 'fan p 3 max 255'
         sshpass -p $PASSWORD ssh -oStrictHostKeyChecking=no -oKexAlgorithms=+diffie-hellman-group14-sha1 $USERNAME@$ILOIP 'fan p 4 max 255'
         sshpass -p $PASSWORD ssh -oStrictHostKeyChecking=no -oKexAlgorithms=+diffie-hellman-group14-sha1 $USERNAME@$ILOIP 'fan p 5 max 255'
-elif [[ $T1 > 58 ]]
+elif [[ $T1 -gt 58 ]]
     then
         sshpass -p $PASSWORD ssh -oStrictHostKeyChecking=no -oKexAlgorithms=+diffie-hellman-group14-sha1 $USERNAME@$ILOIP 'fan p 3 max 39'
         sshpass -p $PASSWORD ssh -oStrictHostKeyChecking=no -oKexAlgorithms=+diffie-hellman-group14-sha1 $USERNAME@$ILOIP 'fan p 4 max 39'
         sshpass -p $PASSWORD ssh -oStrictHostKeyChecking=no -oKexAlgorithms=+diffie-hellman-group14-sha1 $USERNAME@$ILOIP 'fan p 5 max 39'
 
-elif [[ $T1 > 54 ]]
+elif [[ $T1 -gt 54 ]]
     then
         sshpass -p $PASSWORD ssh -oStrictHostKeyChecking=no -oKexAlgorithms=+diffie-hellman-group14-sha1 $USERNAME@$ILOIP 'fan p 3 max 38'
         sshpass -p $PASSWORD ssh -oStrictHostKeyChecking=no -oKexAlgorithms=+diffie-hellman-group14-sha1 $USERNAME@$ILOIP 'fan p 4 max 38'
         sshpass -p $PASSWORD ssh -oStrictHostKeyChecking=no -oKexAlgorithms=+diffie-hellman-group14-sha1 $USERNAME@$ILOIP 'fan p 5 max 38'
-elif [[ $T1 > 52 ]]
+elif [[ $T1 -gt 52 ]]
     then
         sshpass -p $PASSWORD ssh -oStrictHostKeyChecking=no -oKexAlgorithms=+diffie-hellman-group14-sha1 $USERNAME@$ILOIP 'fan p 3 max 34'
         sshpass -p $PASSWORD ssh -oStrictHostKeyChecking=no -oKexAlgorithms=+diffie-hellman-group14-sha1 $USERNAME@$ILOIP 'fan p 4 max 34'
         sshpass -p $PASSWORD ssh -oStrictHostKeyChecking=no -oKexAlgorithms=+diffie-hellman-group14-sha1 $USERNAME@$ILOIP 'fan p 5 max 34'
-elif [[ $T1 > 50 ]]
+elif [[ $T1 -gt 50 ]]
     then
         sshpass -p $PASSWORD ssh -oStrictHostKeyChecking=no -oKexAlgorithms=+diffie-hellman-group14-sha1 $USERNAME@$ILOIP 'fan p 3 max 32'
         sshpass -p $PASSWORD ssh -oStrictHostKeyChecking=no -oKexAlgorithms=+diffie-hellman-group14-sha1 $USERNAME@$ILOIP 'fan p 4 max 32'
@@ -86,28 +87,28 @@ fi
 
 echo "CPU 2 Temp $T2 C"
 
-if [[ $T2 > 67 ]]
+if [[ $T2 -gt 67 ]]
    then
         sshpass -p $PASSWORD ssh -oStrictHostKeyChecking=no -oKexAlgorithms=+diffie-hellman-group14-sha1 $USERNAME@$ILOIP 'fan p 0 max 255'
         sshpass -p $PASSWORD ssh -oStrictHostKeyChecking=no -oKexAlgorithms=+diffie-hellman-group14-sha1 $USERNAME@$ILOIP 'fan p 1 max 255'
         sshpass -p $PASSWORD ssh -oStrictHostKeyChecking=no -oKexAlgorithms=+diffie-hellman-group14-sha1 $USERNAME@$ILOIP 'fan p 2 max 255'
 
-elif [[ $T2 > 58 ]]
+elif [[ $T2 -gt 58 ]]
     then
         sshpass -p $PASSWORD ssh -oStrictHostKeyChecking=no -oKexAlgorithms=+diffie-hellman-group14-sha1 $USERNAME@$ILOIP 'fan p 0 max 39'
         sshpass -p $PASSWORD ssh -oStrictHostKeyChecking=no -oKexAlgorithms=+diffie-hellman-group14-sha1 $USERNAME@$ILOIP 'fan p 1 max 39'
         sshpass -p $PASSWORD ssh -oStrictHostKeyChecking=no -oKexAlgorithms=+diffie-hellman-group14-sha1 $USERNAME@$ILOIP 'fan p 2 max 39'
-elif [[ $T2 > 54 ]]
+elif [[ $T2 -gt 54 ]]
     then
         sshpass -p $PASSWORD ssh -oStrictHostKeyChecking=no -oKexAlgorithms=+diffie-hellman-group14-sha1 $USERNAME@$ILOIP 'fan p 0 max 38'
         sshpass -p $PASSWORD ssh -oStrictHostKeyChecking=no -oKexAlgorithms=+diffie-hellman-group14-sha1 $USERNAME@$ILOIP 'fan p 1 max 38'
         sshpass -p $PASSWORD ssh -oStrictHostKeyChecking=no -oKexAlgorithms=+diffie-hellman-group14-sha1 $USERNAME@$ILOIP 'fan p 2 max 38'
-elif [[ $T2 > 52 ]]
+elif [[ $T2 -gt 52 ]]
     then
         sshpass -p $PASSWORD ssh -oStrictHostKeyChecking=no -oKexAlgorithms=+diffie-hellman-group14-sha1 $USERNAME@$ILOIP 'fan p 0 max 34'
         sshpass -p $PASSWORD ssh -oStrictHostKeyChecking=no -oKexAlgorithms=+diffie-hellman-group14-sha1 $USERNAME@$ILOIP 'fan p 1 max 34'
         sshpass -p $PASSWORD ssh -oStrictHostKeyChecking=no -oKexAlgorithms=+diffie-hellman-group14-sha1 $USERNAME@$ILOIP 'fan p 2 max 34'
-elif [[ $T2 > 50 ]]
+elif [[ $T2 -gt 50 ]]
     then
         sshpass -p $PASSWORD ssh -oStrictHostKeyChecking=no -oKexAlgorithms=+diffie-hellman-group14-sha1 $USERNAME@$ILOIP 'fan p 0 max 32'
         sshpass -p $PASSWORD ssh -oStrictHostKeyChecking=no -oKexAlgorithms=+diffie-hellman-group14-sha1 $USERNAME@$ILOIP 'fan p 1 max 32'
@@ -119,28 +120,28 @@ else
 fi
 
 echo "RAID Temp $RAID C"
-if [[ $RAID > 97 ]]
+if [[ $RAID -gt 97 ]]
    then
         sshpass -p $PASSWORD ssh -oStrictHostKeyChecking=no -oKexAlgorithms=+diffie-hellman-group14-sha1 $USERNAME@$ILOIP 'fan p 5 max 255'
-elif [[ $RAID > 95 ]]
+elif [[ $RAID -gt 95 ]]
     then
         sshpass -p $PASSWORD ssh -oStrictHostKeyChecking=no -oKexAlgorithms=+diffie-hellman-group14-sha1 $USERNAME@$ILOIP 'fan p 5 max 90'
-elif [[ $RAID > 94 ]]
+elif [[ $RAID -gt 94 ]]
     then
         sshpass -p $PASSWORD ssh -oStrictHostKeyChecking=no -oKexAlgorithms=+diffie-hellman-group14-sha1 $USERNAME@$ILOIP 'fan p 5 max 85'
-elif [[ $RAID > 93 ]]
+elif [[ $RAID -gt 93 ]]
     then
         sshpass -p $PASSWORD ssh -oStrictHostKeyChecking=no -oKexAlgorithms=+diffie-hellman-group14-sha1 $USERNAME@$ILOIP 'fan p 5 max 60'
-elif [[ $RAID > 92 ]]
+elif [[ $RAID -gt 92 ]]
     then
         sshpass -p $PASSWORD ssh -oStrictHostKeyChecking=no -oKexAlgorithms=+diffie-hellman-group14-sha1 $USERNAME@$ILOIP 'fan p 5 max 55'
-elif [[ $RAID > 91 ]]
+elif [[ $RAID -gt 91 ]]
     then
         sshpass -p $PASSWORD ssh -oStrictHostKeyChecking=no -oKexAlgorithms=+diffie-hellman-group14-sha1 $USERNAME@$ILOIP 'fan p 5 max 50'
-elif [[ $RAID > 90 ]]
+elif [[ $RAID -gt 90 ]]
     then
         sshpass -p $PASSWORD ssh -oStrictHostKeyChecking=no -oKexAlgorithms=+diffie-hellman-group14-sha1 $USERNAME@$ILOIP 'fan p 5 max 45'
-elif [[ $RAID > 89 ]]
+elif [[ $RAID -gt 89 ]]
     then
         sshpass -p $PASSWORD ssh -oStrictHostKeyChecking=no -oKexAlgorithms=+diffie-hellman-group14-sha1 $USERNAME@$ILOIP 'fan p 5 max 40'
 else
